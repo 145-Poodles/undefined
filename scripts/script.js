33363f62f433870b0fbabde5af2788d1c71b0aab
Nikolai Ivanov <gravitycats@gmail.com>
2016-02-04 21:26:44 +0200

    Finished CMA/Wallpapers page and removed comments

--- scripts/script.js
+++ scripts/script.js
@@ -1,35 +1,4 @@
 $(window).load(function() {
-    // $('ul>li>a').mouseover(function() {
-    //     $(this).css('color', 'black');
-    // });
-    // $('ul>li>a').mouseleave(function() {
-    //     $(this).css('color', '#902634');
-    // });
-    $('.show').click(function() {
-        $('header, main').animate({
-            marginLeft: "+=300px"
-        }, 600);
-        $('aside').animate({
-            left: "+=300px"
-        }, 600);
-        $('.show').hide();
-        setTimeout(function() {
-            $('.hide').show();
-        }, 600)
-    });
-    $('.hide').click(function() {
-        $('header, main').animate({
-            marginLeft: "0"
-        }, 600);
-        $('aside').animate({
-            left: "-=300px"
-        }, 600);
-        console.log('works');
-        $('.hide').hide();
-        setTimeout(function() {
-            $('.show').show();
-        }, 600)
-    });
     $('#box:nth-of-type(1)>img').mouseenter(function() {
         $(this).css('opacity', '0.6');
         $('#box:nth-of-type(1)>span').fadeIn(400);
@@ -54,23 +23,53 @@ $(window).load(function() {
         $(this).css('opacity', '1');
         $('#box:nth-of-type(3)>span').fadeOut(160);
     });
-    $('#box>img').click(function() {
+    $('#box:nth-of-type(1)>img').click(function() {
+        $('#lightbox').hide('<button class="toggle2"></button>');
         $('#lightbox').append('<button class="toggle"></button>');
-        $('#lightbox').append('<img class="large" src="images/iphoneLarge.png">');
-        $('#lightbox').append('<div class="iphone-display-2"></div>');
+        $('#lightbox').append('<img class="large" src="images/iphone.png">');
+        $('#lightbox').append('<div class="iphone-display-1"></div>');
         $('#darkness, #lightbox').fadeIn(400);
         $('.toggle').click(function() {
-            $('#lightbox>div:last').toggleClass('iphone-display-2 iphone-display-1');
+            $('#lightbox>div:last').toggleClass('iphone-display-1 iphone-display-2');
         });
         setTimeout(function() {
-        	$('.toggle').addClass('flick');
-        	setTimeout(function() {
-        		$('.toggle').removeClass('flick');
-        	}, 600)
+            $('.toggle').addClass('flick');
+            setTimeout(function() {
+                $('.toggle').removeClass('flick');
+            }, 600)
+        }, 600)
+    });
+    $('#box:nth-of-type(2)>img').click(function() {
+        $('#lightbox').append('<img class="large" src="images/xperia-white.png">');
+        $('#lightbox').append('<div class="xperia-display-1"></div>');
+        $('#darkness, #lightbox').fadeIn(400);
+        $('.xperia-display-1').click(function() {
+            $('#lightbox>div:last').toggleClass('.xperia-display-1 xperia-display-2');
+        });
+        setTimeout(function() {
+            $('.xperia-display-1').addClass('screen-flick');
+            setTimeout(function() {
+                $('.xperia-display-1').removeClass('screen-flick');
+            }, 600)
+        }, 600)
+    });
+    $('#box:nth-of-type(3)>img').click(function() {
+        $('#lightbox').append('<button class="toggle2"></button>');
+        $('#lightbox').append('<img class="large" src="images/galaxy.png">');
+        $('#lightbox').append('<div class="galaxy-display-1"></div>');
+        $('#darkness, #lightbox').fadeIn(400);
+        $('.toggle2').click(function() {
+            $('#lightbox>div:last').toggleClass('.galaxy-display-1 galaxy-display-2');
+        });
+        setTimeout(function() {
+            $('.toggle2').addClass('flick');
+            setTimeout(function() {
+                $('.toggle2').removeClass('flick');
+            }, 600)
         }, 600)
     });
     $('#darkness').click(function() {
-    	$('#darkness, #lightbox').fadeOut();
+        $('#darkness, #lightbox').fadeOut();
         $('.large').fadeOut();
         $('.iphone-display-2').fadeOut();
         $('.iphone-display-1').fadeOut();
